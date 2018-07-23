ddcd52da8d891a6347a0e08c11ba03f12ca0d4cf
Aman Garg <aman.garg@accoliteindia.com>
2018-07-23 12:10:32 +0530

    Add logger

--- src/main/java/com/accolite/au/springjdbc/SpringJdbcApplication.java
+++ src/main/java/com/accolite/au/springjdbc/SpringJdbcApplication.java
@@ -1,12 +1,16 @@
 package com.accolite.au.springjdbc;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
+	private static final Logger log = Logger.getLogger(SpringJdbcApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJdbcApplication.class, args);
+		log.info("Project is up and running");
 	}
 }
