9e2bb2b4fbb35b9054b6298219140c771af8e8b7
Aman Garg <aman.garg@accoliteindia.com>
2018-07-23 12:05:07 +0530

    Initial build. Change server port to 9002

---
+++ src/main/java/com/accolite/au/springjdbc/SpringJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.accolite.au.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcApplication.class, args);
+	}
+}
